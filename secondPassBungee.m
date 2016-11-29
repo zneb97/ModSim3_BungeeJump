c43ebdf23ac93ccc22133b0e7448e4ca90b09087
Ben Ziemann <benjamin.ziemann@students.olin.edu>
2016-11-28 22:49:32 -0500

    It works! Split equations into four conditionals, one for each velocity direction above and below tension point

--- secondPassBungee.m
+++ secondPassBungee.m
@@ -1,7 +1,7 @@
 function [ T, R ] = secondPassBungee()
 %Spring with air resistance, tension in one direction
 
-A = .7; %Cross-sectional area of human lying down (m^2)
+A = .5; %Cross-sectional area of human lying down (m^2)
 rho = 1.2; %Density of air (kg/m^3)
 Cd = 44.4; %Drag coeffiecent of human lying down (unitless)(data from Google Drive)
 k = 52; %Spring constant of the bungee cord N/m
@@ -18,13 +18,14 @@ Data = [y, vy];
 function res = changingValues(~, Data)
     y = Data(1);
     vy = Data(2);
+    
     dydt = vy;
     if(y < (startH - lo))
-       dvydt = (-(mass1*g) - (k *(y - restingL))+(.5 *rho* Cd* A *vy^2 ))/mass1;
+       dvydt = (-(mass1*g) - (k *(startH-y-lo))-(.5 *rho* Cd* A *vy^2 ))/mass1;
     else
-        dvydt = (-g*mass1 +(.5 *rho* Cd* A *vy^2 ))/mass1;
+        dvydt = (-g*mass1 - (.5 *rho* Cd* A *vy^2 ))/mass1;
     end
-    
+    vy
     res = [dydt; dvydt];
 end
 
@@ -34,19 +35,3 @@ Y = R(:,1);
 figure(2)
 comet(T,Y);
 end
-%Constants
-% 
-%
-% 
-% 
-% A = 0; %Cross-sectional area of human lying down (m^2)
-% rho = 1.2; %Density of air (kg/m^3)
-% g = 9.81; %Gravity acceleration (m/sec^2)
-% Cd = 44.4; %Drag coeffiecent of human lying down (unitless)(data from Google Drive)
-% h = 91.44; %Height of bridge (m) (Sacramento California)
-% 
-% 
-% 
-% 
-% L0 = 45
-% k = 52.2; %Starting k value for bungee (N/kg)
